1b30c986688c0f60e91f141f245bedd9f6868e11
Rubens Kaiserman <rubskaiserman@gmail.com>
2021-07-28 13:37:04 -0300

    Inferencia de tipos

---
+++ cod3r/exercicios/src/fundamentos/InferenciaTipos.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+public class InferenciaTipos {
+    public static void main(String[] args){
+        
+        var mensagem = "Hello, world!";
+
+        System.out.println(mensagem);
+
+        // mensagem = 89; #Não pode
+
+        // int teste = 18;
+        // double vamosVer = teste;
+
+        // System.out.println(vamosVer); # É sim possível salvar um inteiro em uma variavel double, ele será convertido 
+    }
+}
