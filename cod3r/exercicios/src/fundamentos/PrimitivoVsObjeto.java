3870a72920fef31f0b3e46f3b9e4a719cf256242
Rubens Kaiserman <rubskaiserman@gmail.com>
2021-08-03 14:10:50 -0300

    Primitivo vs Objeto

---
+++ cod3r/exercicios/src/fundamentos/PrimitivoVsObjeto.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class PrimitivoVsObjeto{
+    public static void main(String[] args){
+        int num = 3; //Tipo primitivo
+        String string = new String("string"); //classe
+
+        //Tipos primitivos não tem métodos
+        //Tipos primitivos só tem um atributo que é seu valor associado
+        //Wrappers são um tipo de versão objeto dos tipos primitivos
+    }
+}
