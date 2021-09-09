84b3b6746e770c185019fa5c7f80651bb60ac763
Rubens Kaiserman <rubskaiserman@gmail.com>
2021-09-09 12:58:44 -0300

    Operador ternário

---
+++ cod3r/exercicios/src/fundamentos/OperadoresTernarios.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class OperadoresTernarios {
+    public static void main(String[] args) {
+        int y = 29;
+        int x = y <=28 ? 32 : 64;
+
+        System.out.println("X: "+ x);
+    }
+    
+}
