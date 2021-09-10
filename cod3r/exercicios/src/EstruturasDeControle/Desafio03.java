1517203f0d13f0066dcf587f61d678a1bf2c7be8
Rubens Kaiserman <rubskaiserman@gmail.com>
2021-09-10 15:27:38 -0300

    corrigindo caquinha

--- cod3r/exercicios/src/EstruturasDeControle/Desafio03.java
+++ cod3r/exercicios/src/EstruturasDeControle/Desafio03.java
@@ -11,12 +11,14 @@ public class Desafio03 {
         while(val != -1){
             System.out.print("Insira um valor: ");
             val = scan.nextDouble();
-            if((val < 0 || val > 10) && val != -1){
-                System.out.println("Valor inválido!");
-                System.out.println("Tente novamente.");
-            } else {
-                soma += val;
-                total ++;
+            if(val != -1){
+                if(val < 0 || val > 10){
+                    System.out.println("Valor inválido!");
+                    System.out.println("Tente novamente.");
+                } else{
+                    soma += val;
+                    total ++;
+                }
             }
             System.out.println();
         }
