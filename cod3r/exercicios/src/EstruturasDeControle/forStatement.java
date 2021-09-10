a279b71d0803c6660eb82f788c7dfe61e00f101c
Rubens Kaiserman <rubskaiserman@gmail.com>
2021-09-10 14:43:42 -0300

    for statement

---
+++ cod3r/exercicios/src/EstruturasDeControle/forStatement.java
@@ -0,0 +1,9 @@
+package EstruturasDeControle;
+
+public class forStatement {
+    public static void main(String[] args){
+        for(int i = 0; i < 10; i++){
+            System.out.println("Hello World: " + (i + 1));
+        }
+    }
+}
