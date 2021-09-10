a1701a4eadda8759f4b87c64b2cf0899efa81f34
Rubens Kaiserman <rubskaiserman@gmail.com>
2021-09-10 12:16:46 -0300

    Else statement

---
+++ cod3r/exercicios/src/EstruturasDeControle/ElseStatement.java
@@ -0,0 +1,15 @@
+package EstruturasDeControle;
+
+public class ElseStatement {
+    public static void main(String[] args){
+        int x = 17;
+        int y = 100;
+
+        if(x == y){
+            System.out.println("X é igual à Y");
+        } else {
+            System.out.println("X é diferente de Y");
+        }
+    }
+    
+}
