1fb0d29686110e93bdc346854a425bd3b2781782
Rubens Kaiserman <rubskaiserman@gmail.com>
2021-09-10 15:22:04 -0300

    Terceiro desafio

---
+++ cod3r/exercicios/src/EstruturasDeControle/Desafio03.java
@@ -0,0 +1,28 @@
+package EstruturasDeControle;
+
+import java.util.Scanner;
+
+public class Desafio03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double soma = 0;
+        int total = 0;
+        double val = 0;
+        while(val != -1){
+            System.out.print("Insira um valor: ");
+            val = scan.nextDouble();
+            if((val < 0 || val > 10) && val != -1){
+                System.out.println("Valor inválido!");
+                System.out.println("Tente novamente.");
+            } else {
+                soma += val;
+                total ++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total de notas: " + total);
+        System.out.printf("Média: %.2f", soma/total);
+        System.out.println();
+    }
+    
+}
