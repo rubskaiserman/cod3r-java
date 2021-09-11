2ca6e55d56ecb70e2957091a8625f6996420e0dc
Rubens Kaiserman <rubskaiserman@gmail.com>
2021-09-11 11:05:42 -0300

    Desafio concluido com sucesso

---
+++ cod3r/exercicios/src/EstruturasDeControle/Desafio04.java
@@ -0,0 +1,12 @@
+package EstruturasDeControle;
+
+public class Desafio04 {
+    public static void main(String[] args){
+        String string = "#";
+        for(;!string.equals("######");){
+            System.out.println(string);
+            string +="#";
+        }
+    }
+    
+}
