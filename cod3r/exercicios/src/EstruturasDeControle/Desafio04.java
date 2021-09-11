04e93ffe458fe1464e625d79182101fcc904f04f
Rubens Kaiserman <rubskaiserman@gmail.com>
2021-09-11 11:12:40 -0300

    Remoção dos comentarios

--- cod3r/exercicios/src/EstruturasDeControle/Desafio04.java
+++ cod3r/exercicios/src/EstruturasDeControle/Desafio04.java
@@ -2,11 +2,6 @@ package EstruturasDeControle;
 
 public class Desafio04 {
     public static void main(String[] args){
-        // String string = "#";
-        // for(;!string.equals("######");){
-        //     System.out.println(string);
-        //     string +="#";
-        // }
         for(String string = "#"; !string.equals("######"); string += "#"){ //Código mais conciso
             System.out.println(string);
         }
