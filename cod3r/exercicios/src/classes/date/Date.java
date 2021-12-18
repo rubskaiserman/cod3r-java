602182fc15e96b09898332c6a6ff86a9be79ddf4
rubskaiserman <rubskaiserman@gmail.com>
2021-12-18 16:18:06 -0300

    Testando

--- cod3r/exercicios/src/classes/date/Date.java
+++ cod3r/exercicios/src/classes/date/Date.java
@@ -6,6 +6,7 @@ public class Date {
     int day;
 
     public String getFormatDate(){
-        return year + "-" + month + "-" + day;
+        return year + "-" + month + "-" + day; //Admito que achei essa versão mais legivel
+        // return String.format("%d-%d-%d", year, month, day); Achei meio bagunçado mas é uma opção
     }
 }
