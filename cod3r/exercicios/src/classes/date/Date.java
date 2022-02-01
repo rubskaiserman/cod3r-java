e438d94c68bf4d1cfde2ff3f8ac9cda6a8cb7c47
rubskaiserman <rubskaiserman@gmail.com>
2022-02-01 18:07:44 -0300

    Fazendo algumas aulas

--- cod3r/exercicios/src/classes/date/Date.java
+++ cod3r/exercicios/src/classes/date/Date.java
@@ -5,6 +5,19 @@ public class Date {
     int month;
     int day;
 
+    public Date(){
+        this.day = 1;
+        this.month = 1;
+        this.year = 1970;
+        //Dia zero do calendário UNIX   
+
+    }
+    public Date(int year, int month, int day){
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
     public String getFormatDate(){
         return year + "-" + month + "-" + day; //Admito que achei essa versão mais legivel
         // return String.format("%d-%d-%d", year, month, day); Achei meio bagunçado mas é uma opção
--- cod3r/exercicios/src/classes/produto/Produto.java
+++ cod3r/exercicios/src/classes/produto/Produto.java
@@ -1,7 +1,12 @@
 package classes.produto;
 
 public class Produto{
-    String name;
-    double price;
-    double discount;
+    public String name;
+    public double price;
+    public static double discount = 0.25;
+
+    public Produto(String name, double price){
+        this.name = name;
+        this.price = price * (1 - discount);
+    }
 }
