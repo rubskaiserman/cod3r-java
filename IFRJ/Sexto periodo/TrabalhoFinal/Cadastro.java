f824e6e306983221b3ac8ad56655e597120a2c50
rubskaiserman <rubskaiserman@gmail.com>
2022-02-09 16:21:55 -0300

    Algum avanço

--- IFRJ/Sexto periodo/TrabalhoFinal/Cadastro.java
+++ IFRJ/Sexto periodo/TrabalhoFinal/Cadastro.java
@@ -1,18 +1,48 @@
 package TrabalhoFinal;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
+
 
 public class Cadastro{
+
+    private static int getOption(Scanner scan){
+     boolean test = false;
+     int res = -1;
+        
+        while(!test){
+            try {
+                System.out.println("1 - Inserir novo Cliente");
+                System.out.println("2 - Remover registro de cliente");
+                System.out.println("3 - Alterar registro de cliente");
+                System.out.println("4 - Ver registro do cliente");
+                System.out.println("0 - Verificar todos os clientes");
+        
+                res = scan.nextInt();
+                test = res <= 4 && res >=0;
+                if(!test){
+                    System.out.println("Valor não identificado, tente novamente\n \n \n \n \n");
+                }
+                
+            } catch (Exception e) {
+                System.out.println("Valor não identificado, tente novamente");
+            }
+
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         try {  
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/cadastro_clientes", "registrador", "loucuras_de_amor");
-            System.out.println("Conexão feita com sucesso");
             Statement cmd = conn.createStatement();
-            cmd.execute("INSERT INTO clientes(cpf_cliente) VALUES('192912981291');");
+            Scanner scan = new Scanner(System.in);
+            int op = getOption(scan);
+
+
 
 
         } catch (SQLException e) {
--- IFRJ/Sexto periodo/TrabalhoFinal/Cliente.java
+++ IFRJ/Sexto periodo/TrabalhoFinal/Cliente.java
@@ -1,16 +1,20 @@
 package TrabalhoFinal;
 
-import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 
 public class Cliente {
     private int _cpf;
     private String _nome;
     private String _email;
     private String _plano;
-    private Date _dataInscricao;
+    private String _dataInscricao;
     
     
-    public Cliente(int cpf, String nome, String email, Date dataInscricao, String plano){
+    public Cliente(int cpf, String nome, String email, String dataInscricao, String plano){
         this._cpf = cpf;
         this._nome = nome;
         this._dataInscricao = dataInscricao;
@@ -25,29 +29,37 @@ public class Cliente {
     public String getEmail(){
         return this._email;
     }
-    public Date getDate(){
+    public String getDate(){
         return this._dataInscricao;
     }
     public String getPlano(){
         return this._plano;
     }
 
-    public static void registrarCliente(Cliente cliente){
-        //Registra o Cliente na base de dados
+    public static void registrarCliente(Statement statement, Cliente cliente) throws SQLException{
+        String sqlString = String.format("INSERT INTO clientes(cpf_cliente, nome, email, data_inscricao, plano) VALUES(%d, %s, %s, %s, %s)", cliente._cpf, cliente._nome, cliente._email, cliente._dataInscricao, cliente._plano); 
+        statement.execute(sqlString);
 
     }
-    public static void removerCliente(int cpf){
-        //Apaga o cliente da base de dados
+    public static void removerCliente(Statement statement, int cpf) throws SQLException{
+        String sqlString = String.format("DELETE FROM clientes WHERE cpf_cliente = %d", cpf); 
+        statement.execute(sqlString);
 
     }
-    public static void modificarCliente(int cpf, String coluna, String novoValor){
-        //Altera os dados do cliente
+    public static void modificarCliente(Statement statement, int cpf, String coluna, String novoValor) throws SQLException{
+        String sqlString = String.format("UPDATE clientes SET %s = %s WHERE cpf_cliente=%s", coluna, novoValor, cpf); 
+        statement.execute(sqlString);
     }
-    public static void getAllClients(){
+    public static void getAllClients(Statement statement){
         //Retorna todos os clientes
+        
     }
-    public void selectCliente(int cpf){
-        //Retorna um cliente especifico
+    public ResultSet selectCliente(Statement statement, int cpf) throws SQLException{
+        String sqlString = String.format("SELECT FROM clientes WHERE cpf_cliente = %d", cpf); 
+        statement.execute(sqlString);
+        ResultSet clienteSet = statement.getResultSet();
+        // Cliente cliente = new Cliente(clientSet[])
+        return clienteSet;
     }
 }
 
